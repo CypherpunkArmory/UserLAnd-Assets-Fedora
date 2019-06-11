f272c4cdce3baf3e74bbdef6a085f2d7b0cd17cb
Corbin Champion <corbinlc@gmail.com>
2019-06-11 12:29:23 -0700

    make the startVNCServerStep2.sh have a shot at working with tigervnc

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # UserLAnd-Assets-Fedora
 A repository for holding Fedora specific assets for UserLAnd
+
+After cloning this repo, you simply do the following...
+
+`sudo ./scripts/buildArch.sh $desiredArch` 
+
+where `desiredArch` can be `arm`, `arm64`, `x86`, `x86_64`
---
+++ input/main.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+echo "127.0.0.1 localhost" > /etc/hosts
+echo "nameserver 8.8.8.8" > /etc/resolv.conf
+echo "nameserver 8.8.4.4" >> /etc/resolv.conf
+
+echo "#!/bin/sh" > /etc/profile.d/userland.sh
+echo "unset LD_PRELOAD" >> /etc/profile.d/userland.sh
+echo "unset LD_LIBRARY_PATH" >> /etc/profile.d/userland.sh
+echo "export LIBGL_ALWAYS_SOFTWARE=1" >> /etc/profile.d/userland.sh
+chmod +x /etc/profile.d/userland.sh
+
+#update our repos so we can install some packages
+#is anything needed here?
+
+#install some packages with need for UserLAnd
+#dnf install -y --no-install-recommends sudo dropbear libgl1-mesa-glx tightvnc-server xterm xfonts-base twm expect
+dnf install -y sudo dropbear tigervnc-server xterm twm expect
+
+#clean up after ourselves
+dnf clean all
+
+#tar up what we have before we grow it
+tar -czvf /output/rootfs.tar.gz --exclude sys --exclude dev --exclude proc --exclude mnt --exclude etc/mtab --exclude output --exclude input --exclude .dockerenv /
+
+#build disableselinux to go with this release
+dnf install -y @development-tools
+gcc -shared -fpic /input/disableselinux.c -o /output/libdisableselinux.so
+
+#grab a static version of busybox that we can use to set things up later
+dnf install -y busybox
+cp /bin/busybox output/busybox
